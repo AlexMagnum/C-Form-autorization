8203d55b85552678b310e76683f4b9cac8b8b4ec
AlexMagnum <magdubor@gmail.com>
2025-03-26 09:53:41 +0200

    Lesson2
    Lesson2

--- C# Form Autorization/C# Form Autorization/Form1.cs
+++ C# Form Autorization/C# Form Autorization/Form1.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +14,7 @@ namespace C__Form_Autorization
 {
     public partial class Form1: Form
     {
+        SqlConnection conn = new SqlConnection(Properties.Settings.Default.computerConnectionString);
         public Form1()
         { 
             InitializeComponent();
@@ -19,7 +22,46 @@ namespace C__Form_Autorization
 
         private void bunifuMaterialTextbox2_OnValueChanged(object sender, EventArgs e)
         {
-            bunifuMaterialTextbox2.isPassword = true;
+            password.isPassword = true;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void bunifuFlatButton3_Click(object sender, EventArgs e)
+        {
+            Validation();
+        }
+
+        private bool Validation()
+        {
+            if (login.Text.ToString().Trim() == string.Empty ||
+                password.Text.ToString().Trim() == string.Empty)
+            {
+                MessageBox.Show("Будь ласка введіть логін та пароль!", "Не заповнені обов'язкові поля",
+                    MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                return false;
+            }
+            else
+                return true;
+        }
+
+        private string Encrypt(string password)
+        {
+            string salt = "kn221";
+            byte[] data = UTF8Encoding.UTF8.GetBytes(password);
+
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            TripleDESCryptoServiceProvider tripDes = new TripleDESCryptoServiceProvider();
+            tripDes.Key = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(salt));
+            tripDes.Mode = CipherMode.ECB;
+
+            ICryptoTransform transform = tripDes.CreateDecryptor();
+            byte[] result = transform.TransformFinalBlock(data, 0, data.Length);
+
+            return Convert.ToBase64String(result);
         }
     }
 }
